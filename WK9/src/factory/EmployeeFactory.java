497263c399ae3504462097b5b8e5d1bca4e3777b
Natalia <PalejNatalia@gmail.com>
2023-11-06 16:30:43 +0000

    Finished EmployeeFactory and FactoryTester class

--- WK9/src/factory/EmployeeFactory.java
+++ WK9/src/factory/EmployeeFactory.java
@@ -2,4 +2,14 @@ package factory;
 
 public class EmployeeFactory {
 	
+	public static Employee getEmployee(int hoursAllocated, double hourlyRate) {
+		// error checking 
+		
+		if (hoursAllocated >= 40) {
+			return new FullTimeEmployee(hourlyRate);
+		}
+		else {
+			return new PartTimeEmployee(hourlyRate);
+		}
+	}
 }
--- WK9/src/factory/FactoryTester.java
+++ WK9/src/factory/FactoryTester.java
@@ -1,7 +1,16 @@
 package factory;
 
 public class FactoryTester {
+	
 	public static void main(String[] args) {
 		
+		Employee employee = EmployeeFactory.getEmployee(30, 25.0);
+		System.out.println("Hourly rate:\t" + "€"+ employee.getHourlyRate());
+		
+		System.out.println();
+		
+		Employee employee2 = EmployeeFactory.getEmployee(48, 35.0);
+		System.out.println("Hourly rate:\t" + "€"+ employee2.getHourlyRate());
+				
 	}
 }
